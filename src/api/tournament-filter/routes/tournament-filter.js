aa63a1fd2a7f9938768df22e618b567f67fa4058
Graeme Boyes <graeme@insitesdigital.com>
2022-04-14 17:15:27 +0100

    no message

--- src/api/tournament-filter/routes/tournament-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tournament-filter router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::tournament-filter.tournament-filter');
--- src/api/tournament-filter/services/tournament-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tournament-filter service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::tournament-filter.tournament-filter');
